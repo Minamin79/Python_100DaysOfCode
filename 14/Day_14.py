c8067151e54610bb72bd1a36b0049252f7c0a8d9
Mina <139037592+Minamin79@users.noreply.github.com>
2023-07-22 11:04:54 -0400

    Add files via upload

---
+++ 14/Day_14.py
@@ -0,0 +1,41 @@
+import random
+from replit import clear
+from game_data import data
+from art import logo
+
+print(logo)
+
+def game():
+    final_score  = 0
+
+    chosen_a = random.choice(data)
+
+    while True:
+        chosen_b = random.choice(data)
+        if chosen_a == chosen_b:
+            chosen_b = random.choice(data)
+
+        print(f"Compare A: {chosen_a['name']}, a {chosen_a['description']}, from {chosen_a['country']}.")
+        print(f"Against B: {chosen_b['name']}, a {chosen_b['description']}, from {chosen_b['country']}.")
+        
+        guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+        
+        clear()
+        print(logo)
+
+        while guess != 'a' and guess != 'b':
+            print('Invald input')
+            guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+        
+        correct_answer = chosen_a['follower_count'] > chosen_b['follower_count']
+        if correct_answer and guess == 'a' or not correct_answer and guess == 'b':
+            final_score  += 1
+            
+            print(f'Right answer! Current score: {final_score}')
+            chosen_a = chosen_b
+
+        else:
+            print(f'Wrong answer. Your final score: {final_score}')
+            break
+    
+game()
