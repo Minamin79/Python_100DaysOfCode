25dc2c543bef136c322a86fdebd937ca6e3f8412
Mina <139037592+Minamin79@users.noreply.github.com>
2023-07-29 10:32:12 -0400

    Add files via upload

---
+++ 21/Day_21.py
@@ -0,0 +1,51 @@
+# Snake game part 2
+
+from turtle import Screen
+from completed_snake import Snake
+from food import Food
+from scoreboard import ScoreBoard
+import time
+
+snake = Snake()
+food = Food()
+scoreboard = ScoreBoard()
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor('black')
+screen.title('Snake Game')
+screen.tracer(0)
+
+screen.listen()
+screen.onkey(key='Up', fun=snake.up)
+screen.onkey(key='Down', fun=snake.down)
+screen.onkey(key='Left', fun=snake.left)
+screen.onkey(key='Right', fun=snake.right)
+
+snake.create_snake()
+screen.update()
+
+game_on = True
+while game_on:
+    screen.update()
+    time.sleep(0.05)
+    snake.move()
+
+    if snake.head.distance(food) < 15:
+        snake.add_tail_after_food()
+        food.refresh()
+        scoreboard.increase_score()    
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_on = False
+        scoreboard.game_over()
+    
+    for sqr in snake.all_sqr:
+        if sqr == snake.head:
+            pass
+        if sqr == snake.all_sqr[0]:
+            pass
+        elif snake.head.distance(sqr) < 5:
+            print('Bug Saggggg')
+            game_on = False
+            scoreboard.game_over()
+screen.exitonclick()
---
+++ 21/completed_snake.py
@@ -0,0 +1,46 @@
+from turtle import Turtle
+
+class Snake:
+    def __init__(self):
+        self.start_pos = [(0,0), (-20,0), (-40,0)]
+        self.all_sqr = []
+        self.add_tail((0,0))
+        self.head = self.all_sqr[0]
+
+    def create_snake(self):
+        for pos in self.start_pos:
+            self.add_tail(pos)
+
+    def add_tail(self, position):
+        t = Turtle(shape='square')
+        t.color('white')
+        t.penup()
+        t.goto(position)
+        self.all_sqr.append(t)
+
+    def add_tail_after_food(self):
+        self.add_tail(self.all_sqr[-1].position())
+
+    def move(self):
+        for sqr_num in range(len(self.all_sqr)-1, 0, -1):
+            new_x = self.all_sqr[sqr_num - 1].xcor()
+            new_y = self.all_sqr[sqr_num - 1].ycor()
+            self.all_sqr[sqr_num].goto(new_x, new_y)
+            sqr_num = (new_x, new_y)
+        self.head.forward(10)
+
+    def up(self):
+        if self.head.heading() != 270:
+            self.head.setheading(90)
+
+    def down(self):
+        if self.head.heading() != 90:
+            self.head.setheading(270)
+
+    def left(self):
+        if self.head.heading() != 0:
+            self.head.setheading(180)
+
+    def right(self):
+        if self.head.heading() != 180:
+            self.head.setheading(0)
